0526583edb441c1b943b7ccc122c41a1c3b17e5c
Chaz Wilder <cwilder@chazwilder.onmicrosoft.com>
2024-08-30 12:18:07 -0400

    Create struct for database log entries & Organize and export models from the module

---
+++ src/models/mod.rs
@@ -0,0 +1,21 @@
+pub mod plcvalue;
+pub mod idoor;
+pub mod istatus;
+pub mod isensor;
+pub mod ievents;
+pub mod istates;
+pub mod idb_log;
+
+pub use idoor::*;
+pub use istatus::*;
+pub use isensor::*;
+pub use ievents::*;
+pub use istates::*;
+pub use idb_log::*;
+
+use chrono::{Local, NaiveDateTime};
+pub use plcvalue::*;
+
+pub fn local_now() -> NaiveDateTime {
+    Local::now().naive_local()
+}
